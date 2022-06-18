c848e6d1e38ee9df515ef1dd4c8d20cd5e802759
Smarttabdev <87866630+Smarttabdev@users.noreply.github.com>
2022-06-18 10:15:38 -0700

    Svelte project commit

---
+++ src/app.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="@sveltejs/kit" />
+
+// See https://kit.svelte.dev/docs/types#the-app-namespace
+// for information about these interfaces
+declare namespace App {
+	interface Locals {
+		username: string
+	}
+
+	// interface Platform {}
+
+	interface Session {
+		user?: {
+			name: string
+		}
+	}
+
+	// interface Stuff {}
+}
